4e012e54acf31faf0a882b16dbbc3a47b2855983
humanharddrive <humanharddrive@gmail.com>
2020-01-25 21:19:29 -0500

    Add code

--- ConsoleMenu.cpp
+++ ConsoleMenu.cpp
@@ -1,1 +1,58 @@
 #include "ConsoleMenu.h"
+
+#include <cstdlib>
+
+ConsoleMenu::ConsoleMenu(MenuOption* pOptionList) :
+	m_pOptionList(pOptionList),
+	m_pOptionFn(nullptr),
+	m_pOptionData(nullptr),
+	m_bEcho(false)
+{
+}
+
+void ConsoleMenu::update(char c)
+{	
+	if(c == '\e') /*Escape key*/
+	{
+		/*Leave the currently executing option*/
+		m_pOptionFn = nullptr;
+		/*Clean up*/
+		clearScreen();
+		showMenu();
+	}
+	/*Update the currently executing option*/
+	else if(m_pOptionFn)
+	{
+		/*Option functions return true when they're done executing*/
+		if(m_pOptionFn(c, m_pOptionData))
+			m_pOptionFn = nullptr;
+	}
+	else if(c == '\r') /*Return key*/
+	{
+		clearScreen();
+		showMenu();
+	}
+	/*Try to start a new option*/
+	else
+		doMenu(c);
+}
+
+void ConsoleMenu::clearScreen()
+{
+	/*Do something*/
+	/*TeraTerm: \e[2J*/
+}
+
+void ConsoleMenu::showMenu()
+{
+	MenuOption* pOption = m_pOptionList;
+	
+	if(!m_pOption)
+		return;
+	
+	/*Null key indicates the end of a list*/
+	while(pOption->k)
+	{
+		pOption++;
+	}
+}
--- ConsoleMenu.h
+++ ConsoleMenu.h
@@ -1,8 +1,38 @@
 #ifndef __CONSOLE_MENU_H__
 #define __CONSOLE_MENU_H__
 
+/*
+Menu options are formatted as such
+[k] sDisplay
+*/
+struct MenuOption
+{
+	char k; /*Key to trigger the menu option*/
+	char* sDisplay; /*String to display for option*/
+	
+	bool (*fn)(char, void*); /*Function to call when key is hit*/
+	void* pData; /*Pointer to data passed to function*/
+};
+
 class ConsoleMenu
 {
+	public:
+	ConsoleMenu(MenuOption* pOptionList);
+	
+	void update(char c);
+	
+	void clearScreen();
+	void showMenu();
+	
+	void enableEcho() {m_bEcho = true;}
+	void disableEcho() {m_bEcho = false;}
+	
+	private:
+	void doMenu(char c);
+	
+	MenuOption* m_pOptionList;
+	void* m_pOptionData;
+	bool m_bEcho;
 };
 
 #endif
